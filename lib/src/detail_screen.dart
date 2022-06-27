d86ec4d4af70a3aed61063cef53494fbc27c7b2b
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-06-27 19:13:57 +0300

    Fourth Commit

--- lib/src/detail_screen.dart
+++ lib/src/detail_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/mock/mock_data.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+  String appbarTitle;
+
+  DetailScreen({required this.appbarTitle});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+          title: Text(appbarTitle),
+          leading: IconButton(
+            iconSize: 27,
+            icon: const Icon(Icons.arrow_back_rounded),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          backgroundColor: const Color.fromARGB(255, 23, 31, 76)),
     );
   }
 }
--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/mock/mock_data.dart';
+import 'package:flutter_application/src/detail_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -14,8 +15,9 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: const Drawer(),
         appBar: AppBar(
-            title: const Text("My AppBar"),
+            title: const Text("Shop App"),
             backgroundColor: const Color.fromARGB(255, 23, 31, 76)),
         body: GridView.builder(
             itemCount: myData.length,
@@ -23,57 +25,64 @@ class _HomeScreenState extends State<HomeScreen> {
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2, mainAxisSpacing: 20, crossAxisSpacing: 20),
             itemBuilder: (context, index) => Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                      decoration: BoxDecoration(border: Border.all(width: 3)),
-                      height: 350,
-                      child: Image.network(
-                        myData[index]["imageUrl"].toString(),
-                        fit: BoxFit.fill,
-                      )),
-                ),
-                Container(
-                  color: const Color.fromARGB(255, 23, 31, 76),
-                  height: 35,
-                  child: Row(
-                    //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            iconFavorite =
-                                iconFavorite == Icons.favorite_border
-                                    ? Icons.favorite
-                                    : Icons.favorite_border;
-                          });
-                        },
-                        icon: Icon(iconFavorite),
-                        color: Colors.amber,
-                      ),
-                      Expanded(
-                        child: Center(
-                          child: Text(
-                            myData[index]["itemName"].toString(),
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.amber),
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => DetailScreen(
+                              appbarTitle:
+                                  myData[index]["itemName"].toString()),
+                        ));
+                      },
+                      child: Container(
+                          decoration:
+                              BoxDecoration(border: Border.all(width: 3)),
+                          height: 350,
+                          child: Image.network(
+                            myData[index]["imageUrl"].toString(),
+                            fit: BoxFit.fill,
+                          )),
+                    ),
+                    Container(
+                      color: const Color.fromARGB(255, 23, 31, 76),
+                      height: 35,
+                      child: Row(
+                        //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                iconFavorite =
+                                    iconFavorite == Icons.favorite_border
+                                        ? Icons.favorite
+                                        : Icons.favorite_border;
+                              });
+                            },
+                            icon: Icon(iconFavorite),
+                            color: Colors.amber,
                           ),
-                        ),
+                          Expanded(
+                            child: Center(
+                              child: Text(
+                                myData[index]["itemName"].toString(),
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                    color: Colors.amber),
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.shopping_cart_outlined),
+                            color: Colors.amber,
+                          )
+                        ],
                       ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.shopping_cart_outlined),
-                        color: Colors.amber,
-                      )
-                    ],
-                  ),
-                )
-              ],
-            )));
+                    )
+                  ],
+                )));
   }
 }
