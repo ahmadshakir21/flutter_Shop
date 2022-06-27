8417d01c9a05c9039e4b28a89d46ecfb6a02421d
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-06-27 18:58:18 +0300

    Second Commit

---
+++ lib/mock/mock_data.dart
@@ -0,0 +1,111 @@
+const myData = [
+  {
+    "id": 1,
+    "itemName": "Bike",
+    "description": "this is a Bike",
+    "imageUrl":
+        "https://p.vitalmtb.com/photos/users/23425/setup_checks/21148/photos/8412/original_IMG_6125.jpg?1365345789",
+    "price": "${82}\$",
+    "color": ["red", "blue", "green", "black", "white", "grey"],
+    "made": "Japan"
+  },
+
+  {
+    "id": 2,
+    "itemName": "Motorcycle",
+    "description": "this is a Motorcycle",
+    "imageUrl":
+        "https://tse1.explicit.bing.net/th?id=OIP.K65LN36UeIHOY6wr8Z3m7gHaE7&pid=Api&P=0&w=274&h=182",
+    "price": "${365}\$",
+    "color": ["red", "green", "black", "white", "yellow"],
+    "made": "Italy"
+  },
+
+  {
+    "id": 3,
+    "itemName": "Scooter",
+    "description": "this is a Scooter",
+    "imageUrl":
+        "https://tse1.mm.bing.net/th?id=OIP.4vlsrdkNMSP5Nz67yeylLQHaHa&pid=Api&P=0&w=170&h=170",
+    "price": "${36}\$",
+    "color": ["green", "black", "white", "grey"],
+    "made": "Germany"
+  },
+
+  {
+    "id": 4,
+    "itemName": "Audi",
+    "description": "this is a Audi",
+    "imageUrl":
+        "http://2.bp.blogspot.com/-6SSW1A2oz_I/UtK0uCasu7I/AAAAAAAAGGg/-Da5apg8THA/s1600/latest+cars+in+2014+12.jpg",
+    "price": "${45120}\$",
+    "color": ["red", "blue", "black", "white"],
+    "made": "Germany"
+  },
+
+  {
+    "id": 5,
+    "itemName": "Gucci",
+    "description": "this is a Gucci",
+    "imageUrl":
+        "https://tse1.mm.bing.net/th?id=OIP.yRf-HQPL0o47AeTzpQ6WLgHaHa&pid=Api&P=0&w=165&h=165",
+    "price": "${12}\$",
+    "color": ["red", "blue", "green", "black", "white", "grey" , "yellow"],
+    "made": "Italy"
+  },
+
+  {
+    "id": 6,
+    "itemName": "Shirt",
+    "description": "this is a Shirt",
+    "imageUrl":
+        "https://tse3.mm.bing.net/th?id=OIP.iUclpABauV7_F5AXloO-BQHaHa&pid=Api&P=0&w=185&h=185",
+    "price": "${100}\$",
+    "color": ["red", "blue", "green", "yellow" ,"black", "white", "grey"],
+    "made": "Turkey"
+  },
+
+  {
+    "id": 7,
+    "itemName": "Soccer Ball",
+    "description": "this is a Soccer Ball",
+    "imageUrl":
+        "https://tse3.mm.bing.net/th?id=OIP.jdt8VNtyICulU4jtOgoakAHaG5&pid=Api&P=0&w=178&h=166",
+    "price": "${42}\$",
+    "color": ["red", "blue", "green", "black", "white", "grey"],
+    "made": "Germany"
+  },
+
+  {
+    "id": 8,
+    "itemName": "Tennis & Ball",
+    "description": "this is a Tennis & Ball",
+    "imageUrl":
+        "https://tse4.mm.bing.net/th?id=OIP.IfLVrvi1VjTU6sWwM9hxewHaEL&pid=Api&P=0&w=278&h=157",
+    "price": "${33}\$",
+    "color": ["blue", "green", "black", "white"],
+    "made": "Japan"
+  },
+
+  {
+    "id": 9,
+    "itemName": "MSI Personal Computer",
+    "description": "this is a MSI Personal Computer",
+    "imageUrl":
+        "https://tse2.mm.bing.net/th?id=OIP.h29GW8pX32ONehf6dW6EwQHaEK&pid=Api&P=0&w=279&h=157",
+    "price": "${1350}\$",
+    "color": ["black", "white", "grey"],
+    "made": "Taiwan"
+  },
+
+  {
+    "id": 10,
+    "itemName": "Headphone",
+    "description": "this is a Headphone",
+    "imageUrl":
+        "https://www.jasani.ae/web/image/product.image/444/image_1024/POZNAN%20-%20Giftology%20Bluetooth%20Headphone?unique=ccf341e",
+    "price": "${60}\$",
+    "color": ["red", "blue", "green", "black", "white", "grey"],
+    "made": "Belgium"
+  },
+];
---
+++ lib/src/home_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/mock/mock_data.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  IconData iconFavorite = Icons.favorite_border;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text("My AppBar"),
+            backgroundColor: const Color.fromARGB(255, 23, 31, 76)),
+        body: GridView.builder(
+            itemCount: myData.length,
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 15),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 20, crossAxisSpacing: 20),
+            itemBuilder: (context, index) => Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                      decoration: BoxDecoration(border: Border.all(width: 3)),
+                      height: 350,
+                      child: Image.network(
+                        myData[index]["imageUrl"].toString(),
+                        fit: BoxFit.fill,
+                      )),
+                ),
+                Container(
+                  color: const Color.fromARGB(255, 23, 31, 76),
+                  height: 35,
+                  child: Row(
+                    //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            iconFavorite =
+                                iconFavorite == Icons.favorite_border
+                                    ? Icons.favorite
+                                    : Icons.favorite_border;
+                          });
+                        },
+                        icon: Icon(iconFavorite),
+                        color: Colors.amber,
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: Text(
+                            myData[index]["itemName"].toString(),
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.amber),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.shopping_cart_outlined),
+                        color: Colors.amber,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )));
+  }
+}
